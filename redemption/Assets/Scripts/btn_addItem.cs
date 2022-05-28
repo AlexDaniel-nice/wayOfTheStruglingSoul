d972585e0a25c37f0720e581d4b1b39a154ee0b2
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-05-28 14:34:38 +0300

    working on adding items

--- redemption/Assets/Scripts/btn_addItem.cs
+++ redemption/Assets/Scripts/btn_addItem.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class btn_manager : MonoBehaviour
+public class btn_addItem : MonoBehaviour
 {
-    [SerializeField] private GameObject Pnl_addItem;
-    [SerializeField] private GameObject Pnl_addItem;
+    //[SerializeField] private GameObject Pnl_addItem;
     // Start is called before the first frame update
     void Start()
     {
