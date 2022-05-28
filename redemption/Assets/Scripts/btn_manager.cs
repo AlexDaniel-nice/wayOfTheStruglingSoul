45df68fec303eae2582a71336410519bba5c74bb
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-05-28 14:27:20 +0300

    Stabel version 1
    new names
    menus are linked

--- redemption/Assets/Scripts/btn_manager.cs
+++ redemption/Assets/Scripts/btn_manager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hasHeldTask : MonoBehaviour
+public class btn_manager : MonoBehaviour
 {
+    [SerializeField] private GameObject Pnl_addItem;
+    [SerializeField] private GameObject Pnl_addItem;
     // Start is called before the first frame update
     void Start()
     {
