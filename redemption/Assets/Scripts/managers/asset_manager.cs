132a20b3e3665249fe29c7f110302b5e767eeba5
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-05-28 15:35:22 +0300

    experimental spawn objects

--- redemption/Assets/Scripts/managers/asset_manager.cs
+++ redemption/Assets/Scripts/managers/asset_manager.cs
@@ -6,7 +6,17 @@ using UnityEngine;
 public class asset_manager : MonoBehaviour
 {
     [SerializeField] private GameObject task_template;
-    
+
+    private static asset_manager _i;
+
+    private static asset_manager i
+    {
+        get
+        {
+            if (_i == null) _i = (Instantiate(Resources.Load("mng_asset")) as GameObject).GetComponent<asset_manager>();
+            return _i;
+        }
+    }
 
 
 }
