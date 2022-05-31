fdf00e6bc68221621bf01454e9acfd4512455b89
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-05-31 20:44:50 +0300

    We can add to list kindda....

--- redemption/Assets/Scripts/createTask.cs
+++ redemption/Assets/Scripts/createTask.cs
@@ -2,27 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class createTask : MonoBehaviour
 {
+    //pt event
     [SerializeField] GameObject btn_manager;
-    [SerializeField] GameObject task_template;
+
+    //pt a obtine prefab-ul pt task
+    [SerializeField] GameObject PF_task;
+
+    //pt a obine parintele final
     [SerializeField] Transform task_content;
 
+    GameObject task_text;
+    TMP_Text txt;
     btns_manager clickedAccept;
+    TMP_InputField task_textEditor;
+
     void Start()
     {
+        task_textEditor = GetComponent<TMP_InputField>();
+        
+        task_text = PF_task.transform.GetChild(0).gameObject;
+        txt = task_text.GetComponent<TMP_Text>();
+
         clickedAccept = btn_manager.GetComponent<btns_manager>();
         clickedAccept.onAcceptBtnPressed += ClickedAccept_onAcceptBtnPressed;      
     }
 
     private void ClickedAccept_onAcceptBtnPressed(object sender, System.EventArgs e)
     {
-        GameObject task;
+        GameObject new_task;
 
-        task = Instantiate(task_template);
-        task.transform.SetParent(task_content);
-        task.transform.localScale = task_content.localScale;
+        new_task = Instantiate(PF_task);
+        new_task.transform.SetParent(task_content);
+        new_task.transform.localScale = task_content.localScale;
+
+        txt.text = task_textEditor.text;
+
         Debug.Log("Boing");
+        Debug.Log(task_textEditor.text);
     }
 }
