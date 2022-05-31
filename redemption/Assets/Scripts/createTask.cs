791ac1de67de77f7e5688b531ed9d2e6e3bf0b47
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-05-31 20:49:35 +0300

    Core app is done

--- redemption/Assets/Scripts/createTask.cs
+++ redemption/Assets/Scripts/createTask.cs
@@ -21,25 +21,28 @@ public class createTask : MonoBehaviour
     btns_manager clickedAccept;
     TMP_InputField task_textEditor;
 
-    void Start()
+    //initializare
+    private void Awake()
     {
         task_textEditor = GetComponent<TMP_InputField>();
-        
-        task_text = PF_task.transform.GetChild(0).gameObject;
-        txt = task_text.GetComponent<TMP_Text>();
 
         clickedAccept = btn_manager.GetComponent<btns_manager>();
+    }
+    void Start()
+    {
         clickedAccept.onAcceptBtnPressed += ClickedAccept_onAcceptBtnPressed;      
     }
 
     private void ClickedAccept_onAcceptBtnPressed(object sender, System.EventArgs e)
     {
         GameObject new_task;
-
         new_task = Instantiate(PF_task);
         new_task.transform.SetParent(task_content);
         new_task.transform.localScale = task_content.localScale;
 
+        task_text = new_task.transform.GetChild(0).gameObject;
+        txt = task_text.GetComponent<TMP_Text>();
+        
         txt.text = task_textEditor.text;
 
         Debug.Log("Boing");
