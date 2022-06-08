b7db09449fcb7ce4ffaaefc5d0ff44edef4bc8d2
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-06-08 12:24:25 +0300

    optimizari

--- redemption/Assets/Scripts/createTask.cs
+++ redemption/Assets/Scripts/createTask.cs
@@ -5,37 +5,33 @@ using UnityEngine;
 public class createTask : MonoBehaviour
 {
     //pt event
-    [SerializeField] GameObject btn_manager;
+    [SerializeField] btns_manager addEvent;
 
     //pt a obtine prefab-ul pt task
     [SerializeField] GameObject PF_task;
 
     //pt a obine parintele final
-    [SerializeField] Transform task_content;
+    [SerializeField] Transform task_panel;
 
     GameObject task_text;
     TMP_Text txt;
-    btns_manager clickedAccept;
     TMP_InputField task_textEditor;
 
     //initializare
     private void Awake()
     {
         task_textEditor = GetComponent<TMP_InputField>();
-
-        clickedAccept = btn_manager.GetComponent<btns_manager>();
-    }
-    void Start()
-    {
-        clickedAccept.onAcceptBtnPressed += ClickedAccept_onAcceptBtnPressed;
+        addEvent.onAcceptBtnPressed += addEvent_onAcceptBtnPressed;
     }
 
-    private void ClickedAccept_onAcceptBtnPressed(object sender, System.EventArgs e)
+    private void addEvent_onAcceptBtnPressed(object sender, System.EventArgs e)
     {
         GameObject new_task;
+
+        //initializare template
         new_task = Instantiate(PF_task);
-        new_task.transform.SetParent(task_content);
-        new_task.transform.localScale = task_content.localScale;
+        new_task.transform.SetParent(task_panel);
+        new_task.transform.localScale = task_panel.localScale;
 
         task_text = new_task.transform.GetChild(0).gameObject;
         txt = task_text.GetComponent<TMP_Text>();
